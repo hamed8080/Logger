5e43ce05b1247a5fc0e725e591e4b5b961dce3e9
Hamed Hosseini <hamed8080@gmail.com>
2024-05-20 12:52:31 +0330

    Package: Use local path of SDKs in development environment

--- Package.swift
+++ Package.swift
@@ -17,24 +17,24 @@ let package = Package(
             targets: ["Logger"]),
     ],
     dependencies: [
-        .package(url: "https://pubgi.fanapsoft.ir/chat/ios/additive", from: "1.2.0"),
-        .package(url: "https://pubgi.fanapsoft.ir/chat/ios/mocks", from: "1.2.0"),
+        .package(path: "../Additive"),
+        .package(path: "../Mocks"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "Logger",
             dependencies: [
-                .product(name: "Additive", package: "additive"),
-                .product(name: "Mocks", package: "mocks"),
+                .product(name: "Additive", package: "Additive"),
+                .product(name: "Mocks", package: "Mocks"),
             ],
             resources: [.process("Resources")]
         ),
         .testTarget(
             name: "LoggerTests",
             dependencies: [
-                .product(name: "Additive", package: "additive"),
-                .product(name: "Mocks", package: "mocks"),
+                .product(name: "Additive", package: "Additive"),
+                .product(name: "Mocks", package: "Mocks"),
                 "Logger",
             ]
         ),
