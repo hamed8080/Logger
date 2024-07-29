hamed8080/chat

Use a comment version along side with repo version to switch faster

Package.swift
@@ -14,20 +14,23 @@ let package = Package(
         .library(name: "Chat", targets: ["Chat"]),
     ],
     dependencies: [
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-extensions", from: "2.1.0"),
+//        .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-extensions", from: "2.1.0"),
+           .package(path: "../ChatExtensions"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "Chat",
             dependencies: [
-                .product(name: "ChatExtensions", package: "chat-extensions"),
+//                .product(name: "ChatExtensions", package: "chat-extensions"),
+                .product(name: "ChatExtensions", package: "ChatExtensions"),
             ],
             resources: []
         ),
         .testTarget(name: "ChatTests", dependencies: [
             "Chat",
-            .product(name: "ChatExtensions", package: "chat-extensions"),
+//            .product(name: "ChatExtensions", package: "chat-extensions"),
+            .product(name: "ChatExtensions", package: "ChatExtensions"),
         ], path: "Tests"),
     ]
 )

hamed8080/Logger

Package.swift
@@ -17,24 +17,30 @@ let package = Package(
             targets: ["Logger"]),
     ],
     dependencies: [
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/additive", from: "1.2.2"),
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/mocks", from: "1.2.3"),
+//        .package(url: "https://pubgi.sandpod.ir/chat/ios/additive", from: "1.2.2"),
+//        .package(url: "https://pubgi.sandpod.ir/chat/ios/mocks", from: "1.2.3"),
+        .package(path: "../Additive"),
+        .package(path: "../Mocks"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "Logger",
             dependencies: [
-                .product(name: "Additive", package: "additive"),
-                .product(name: "Mocks", package: "mocks"),
+//                .product(name: "Additive", package: "additive"),
+//                .product(name: "Mocks", package: "mocks"),
+                .product(name: "Additive", package: "Additive"),
+                .product(name: "Mocks", package: "Mocks"),
             ],
             resources: [.process("Resources")]
         ),
         .testTarget(
             name: "LoggerTests",
             dependencies: [
-                .product(name: "Additive", package: "additive"),
-                .product(name: "Mocks", package: "mocks"),
+//                .product(name: "Additive", package: "additive"),
+//                .product(name: "Mocks", package: "mocks"),
+                .product(name: "Additive", package: "Additive"),
+                .product(name: "Mocks", package: "Mocks"),
                 "Logger",
             ]
         ),