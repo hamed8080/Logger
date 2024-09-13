1fe5c0a2083b1c5ebbb515a717ed6be76a387b7f
Hamed Hosseini <hamed8080@gmail.com>
2024-09-13 10:53:05 +0330

    Fixed macOS version for building in command-line environment

--- Logger.podspec
+++ Logger.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "Logger"
-  s.version      = "1.2.3"
+  s.version      = "1.2.4"
   s.summary      = "Logger SDK"
   s.description  = "Logging on a log server or device in a unified manner."
   s.homepage     = "https://pubgi.sandpod.ir/chat/ios/logger"
@@ -12,6 +12,6 @@ Pod::Spec.new do |s|
   s.source_files = "Sources/Logger/**/*.{h,swift,xcdatamodeld,m,momd}"
   s.resources = "Sources/Logger/Resources/*.xcdatamodeld"
   s.frameworks  = "Foundation" , "CoreData"
-  s.dependency "Additive" , '~> 1.2.2'
-  s.dependency "Mocks" , '~> 1.2.3'
+  s.dependency "Additive" , '~> 1.2.4'
+  s.dependency "Mocks" , '~> 1.2.5'
 end
--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let local: [Package.Dependency] = [
 ]
 
 let remote: [Package.Dependency] = [
-    .package(url: "https://pubgi.sandpod.ir/chat/ios/additive", from: "1.2.3"),
-    .package(url: "https://pubgi.sandpod.ir/chat/ios/mocks", from: "1.2.4"),
+    .package(url: "https://pubgi.sandpod.ir/chat/ios/additive", from: "1.2.4"),
+    .package(url: "https://pubgi.sandpod.ir/chat/ios/mocks", from: "1.2.5"),
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
 ]
 
@@ -22,7 +22,7 @@ let package = Package(
     defaultLocalization: "en",
     platforms: [
         .iOS(.v10),
-        .macOS(.v12),
+        .macOS(.v10_13),
         .macCatalyst(.v13),
     ],
     products: [
